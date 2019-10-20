4cc081b9445b42e2e887575400182f9bdeab76a3
KarloFab <fabijanickarlo1@gmail.com>
2019-10-20 21:51:14 +0200

    Added initial project

---
+++ src/main/java/com/karlo/springrestwebflux/SpringRestWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.karlo.springrestwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestWebfluxApplication.class, args);
+	}
+
+}
