410f0b0b559291763e2304fff737115041e7e691
KarloFab <fabijanickarlo1@gmail.com>
2019-10-23 22:07:32 +0200

    Added Vendor Controller

---
+++ src/main/java/com/karlo/springrestwebflux/controller/VendorController.java
@@ -0,0 +1,32 @@
+package com.karlo.springrestwebflux.controller;
+
+import com.karlo.springrestwebflux.domain.Category;
+import com.karlo.springrestwebflux.domain.Vendor;
+import com.karlo.springrestwebflux.repository.VendorRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/v1")
+public class VendorController {
+
+    private VendorRepository vendorRepository;
+
+    public VendorController(VendorRepository vendorRepository) {
+        this.vendorRepository = vendorRepository;
+    }
+
+    @GetMapping("/vendors")
+    Flux<Vendor> getAll(){
+        return vendorRepository.findAll();
+    }
+
+    @GetMapping("/vendors/{id}")
+    Mono<Vendor> getById(@PathVariable String id){
+        return vendorRepository.findById(id);
+    }
+}
